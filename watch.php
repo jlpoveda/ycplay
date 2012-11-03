adb0b18d5b73872458056f5cbf869507790367ed
Jose Luis <jlpoveda@mint.(none)>
2012-11-03 10:27:47 +0100

    El search casi funcionando

--- watch.php
+++ watch.php
@@ -35,36 +35,11 @@
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
           <a class="brand" href="#">Project name</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#about">About</a></li>
-              <li><a href="#contact">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li class="divider"></li>
-                  <li class="nav-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="navbar-form pull-right">
-              <input class="span2" type="text" placeholder="Email">
-              <input class="span2" type="password" placeholder="Password">
-              <button type="submit" class="btn">Sign in</button>
-            </form>
-          </div><!--/.nav-collapse -->
+          <form class="navbar-form pull-left" action="search.php" method="get">
+            <input class="span2" type="text" placeholder="Title" name="search_query">
+            <button type="submit" class="btn">Search</button>
+          </form>
         </div>
       </div>
     </div>
@@ -76,10 +51,17 @@
         <div class="span1">
           <h1>Play</h1>
         </div>
-        <div class="span10">
-          <h1>Hello, world!</h1>
-          <p>This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
-          <p><a class="btn btn-primary btn-large">Learn more &raquo;</a></p>
+        <div class="span2">
+        </div>
+        <div class="span6">
+          <object width="640" height="360">
+            <param name="movie" value="https://www.youtube.com/v/Zhawgd0REhA?version=3"></param>
+            <param name="allowFullScreen" value="true"></param>
+            <param name="allowScriptAccess" value="always"></param>
+            <embed src="https://www.youtube.com/v/Zhawgd0REhA?version=3" type="application/x-shockwave-flash" allowfullscreen="true" allowScriptAccess="always" width="640" height="360"></embed>
+          </object>
+        </div>
+        <div class="span2">
         </div>
         <div class="span1">
           <h1>Nxt</h1>
