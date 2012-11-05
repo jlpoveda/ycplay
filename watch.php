33af7482dfc722b3e0e8648f6f57431467865c94
Jose Luis <joseluis@openhousegroup.com>
2012-11-05 17:23:47 +0100

    Agregado el Google Analytics

--- watch.php
+++ watch.php
@@ -147,5 +147,18 @@ $nextVideoId = $videoIds[rand(0, (count($result['data']['items'])-1))];
       });
 
     </script>
+    <script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-3155832-7']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>    
   </body>
 </html>
